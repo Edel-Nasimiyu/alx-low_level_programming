a7b74aff6ebf26431d794a402a8a8118c15bad62
Edel-Nasimiyu <edelnasimiyu354@gmail.com>
2023-02-16 07:55:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints out a s
+ * Return: 0 if success
+ */
+int main(void0
+		{
+		puts("\"Programming is like building a multilingual puzzle
+				return (0)
+				}
