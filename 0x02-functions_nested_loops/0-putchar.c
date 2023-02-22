371852e92817357740a8d6a7076efc46d54c07a0
Edel-Nasimiyu <edelnasimiyu354@gmail.com>
2023-02-22 03:53:21 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * main - Entry point
+ * Description: main - print _putchar
  * Return: 0.
  */
 int main(void)
 {
-char *sh = "Main";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
