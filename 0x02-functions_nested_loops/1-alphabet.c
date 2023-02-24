7d35a7c80108e49837189fa546a8620a485d682a
Edel-Nasimiyu <edelnasimiyu354@gmail.com>
2023-02-24 09:27:56 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: 0.
+ * print_alphabet - print all alphabet in lowercase
  */
 void print_alphabet(void)
 {
-int n;
-for (n = 'a'; n <= 'z'; n++}
-{
-_putchar(n);
-}
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 _putchar('\n');
 }
