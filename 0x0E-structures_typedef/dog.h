5338fae4cfd741634fb149b8d74ad822bdf6b569
Edel-Nasimiyu <edelnasimiyu354@gmail.com>
2023-03-20 07:14:43 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,27 @@
-#include "dog.h"
+#ifndef DOGH
+#define DOGH
+
 /**
- * init_dog - Entry point
- * @d: dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * struct dog - a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+typedef struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+typedef dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
+#endif
